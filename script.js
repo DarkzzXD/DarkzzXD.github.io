8251f3115e7710bc6ff282973e539c6b2e9df9de
ItzDarkzXD <DarkzzXD@users.noreply.github.com>
2024-08-05 15:50:54 -0300

    removed comments

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 function logCenteredText(text) {
-    const consoleWidth = 80; // Assuming a typical console width of 80 characters
+    const consoleWidth = 80;
     const padding = Math.max(0, Math.floor((consoleWidth - text.length) / 2));
     const paddedText = ' '.repeat(padding) + text + ' '.repeat(padding);
     console.log(`%c${paddedText}`, 'font-size: 16px; font-family: "Comic Sans MS", "Comic Sans", cursive;');
 }
 
-// Logging centered text
 logCenteredText('          Oh hey!');
 logCenteredText('Watcha doing here bro?');
 
@@ -17,11 +16,11 @@ function sineInOut(t) {
     return -(Math.cos(Math.PI * t) - 1) / 2;
 }
 
-let startColor1 = [59, 0, 102]; // Initial gradient start color
-let startColor2 = [0, 0, 0]; // Initial gradient end color
-let endColor1 = [179, 0, 255]; // Secondary gradient start color for larger range
-let endColor2 = [0, 0, 0]; // Secondary gradient end color for larger range
-let duration = 5000; // 1 second
+let startColor1 = [59, 0, 102];
+let startColor2 = [0, 0, 0];
+let endColor1 = [179, 0, 255];
+let endColor2 = [0, 0, 0];
+let duration = 5000;
 let startTime;
 
 function animateGradient(timestamp) {
@@ -53,15 +52,15 @@ requestAnimationFrame(animateGradient);
 
 function createDots() {
     const dotsContainer = document.getElementById('dots-container');
-    const numDots = 30; // Number of dots
+    const numDots = 30;
     const dots = [];
-    const margin = 50; // Margin from edges to prevent scrollbars
+    const margin = 50;
 
     for (let i = 0; i < numDots; i++) {
         const dot = document.createElement('div');
         dot.classList.add('dot');
         
-        // Set initial random position
+
         dot.style.top = `${Math.random() * (document.body.scrollHeight - margin * 2) + margin + 1}px`;
         dot.style.left = `${Math.random() * (document.body.scrollWidth - margin * 2) + margin}px`;
 
@@ -69,7 +68,7 @@ function createDots() {
         dots.push(dot);
     }
 
-    // Move dots to random positions
+
     function moveDots() {
         dots.forEach(dot => {
             const newX = Math.random() * (document.body.scrollWidth - margin * 2) + margin;
@@ -77,7 +76,7 @@ function createDots() {
             animateDot(dot, newX, newY);
         });
 
-        // Repeat the movement
+
         setTimeout(moveDots, 5000);
     }
 
